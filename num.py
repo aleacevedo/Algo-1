7697380077538862e4dd62358778fb19e5e5fec2
ale.acevedo <ale.acevedo@live.com.ar>
2016-05-15 15:51:41 -0300

    Commit Message

---
+++ num.py
@@ -0,0 +1,21 @@
+def NumCuriosos(Num):
+	x=0
+	while (True):
+		print(Hola)
+		x = x+1
+		verdad=True
+		if(cant<Num)
+			portencia=x**2
+			resultado=str(portencia)
+			numero=str(x)
+			indice=len(resultado)-len(numero)
+			for h in range(len(numero)-1;-1;-1):
+				if(numero[h]!=resultado[h+indice]):
+					verdad=false
+			if(verdad):
+				print(x)
+				cant=cant+1
+		else break
+	return
+
+NumCuriosos(10)
